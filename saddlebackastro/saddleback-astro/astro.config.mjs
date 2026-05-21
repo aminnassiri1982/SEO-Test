3f1919c769fbd504fb66f97b6dad42156a0eae96
Dentist Nerds <amin@dentistnerds.com>
2026-05-20 22:37:34 -0700

    Delete saddlebackastro directory

--- saddlebackastro/saddleback-astro/astro.config.mjs
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-  site: 'https://saddlebackvalleyendodontics.com',
-  // Pretty URLs: /blog/post-slug/ instead of /blog/post-slug.html
-  trailingSlash: 'always',
-});
